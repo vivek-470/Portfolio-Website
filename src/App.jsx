9d4efc5f53e3e037d2d2b17cbe9caae6471c9046
vivek-470 <vk9704599@gmail.com>
2025-04-24 14:59:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, BrowserRouter } from "react-router";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import { motion } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-white h-screen ">
+      <motion.div
+        initial={{ opacity: 0, y: -150 }}
+        animate={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+        <Home />
+        <Skills />
+        <Portfolio />
+        <Experience />
+        <Contact />
+      </motion.div>
+      <Navbar />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import spotify from '../assets/Images/spotify.png'
+import { FaGithub } from "react-icons/fa";
+
+const Portfolio = () => {
+  return (
+    <section  id='portfolio' >
+      <div className='mt-6'  id='portfolio'>
+      <h1 className='text-black font-bold text-2xl md:w-[700px] max-w-xl flex justify-center items-center'>Portfolio</h1>
+      {/* project 1 */}
+      <div className='flex justify-center md:items-center md:gap-10  flex-col items-center md:flex-row max-w-[480px] md:max-w-full '>
+      <div className=' mt-[-80px] h-[450px]'>
+        <img className="md:h-[550px] h-[450px] md:w-[500px] w-[450px] object-contain rounded-sm  " src={spotify} alt="" />
+
+      </div>
+      <div className=' mt-7 h-[300px] flex justify-center  flex-col gap-7 '>
+       <h1 className='font-semibold text-2xl'>Project #1</h1>
+       <h1 className='font-semibold text-2xl'>Spotify clone using React</h1>
+       <div className='flex gap-5 '>
+        <button className='h-[50px] w-[120px] bg-purple-500 rounded-2xl text-white font-semibold cursor-pointer hover:bg-purple-600'>View Site</button>
+        <button className='h-[50px] w-[50px] bg-purple-500 rounded-2xl text-white font-semibold flex justify-center items-center cursor-pointer hover:bg-purple-600'><FaGithub className='text-2xl '/></button>
+       </div>
+
+      </div>
+      </div>
+      {/* project 2 */}
+      <div className='flex justify-center md:items-center md:gap-10  flex-col items-center md:flex-row max-w-[480px] md:max-w-full '>
+     
+      <div className=' mt-7 h-[300px] flex justify-center  flex-col gap-7 '>
+       <h1 className='font-semibold text-2xl'>Project #2</h1>
+       <h1 className='font-semibold text-2xl'>Spotify clone using React</h1>
+       <div className='flex gap-5 '>
+        <button className='h-[50px] w-[120px] bg-purple-500 rounded-2xl text-white font-semibold cursor-pointer hover:bg-purple-600'>View Site</button>
+        <button className='h-[50px] w-[50px] bg-purple-500 rounded-2xl text-white font-semibold flex justify-center items-center cursor-pointer hover:bg-purple-600'><FaGithub className='text-2xl '/></button>
+       </div>
+
+      </div>
+      <div className=' mt-[-80px] h-[450px] '>
+        <img className="md:h-[550px] h-[450px] md:w-[500px] w-[450px] object-contain rounded-sm " src={spotify} alt="" />
+
+      </div>
+      </div>
+    </div>
+    </section>
+  )
+}
+
+export default Portfolio
