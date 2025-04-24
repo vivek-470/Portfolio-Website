960221a65863bc73784958e09399ee031984a2fe
vivek-470 <vk9704599@gmail.com>
2025-04-24 15:02:37 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Route, Routes, BrowserRouter } from "react-router";
 import Portfolio from "./components/Portfolio";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
