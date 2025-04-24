9278096fd9f041690cb24a5be7d3751d458b68ee
vivek-470 <vk9704599@gmail.com>
2025-04-24 15:22:03 +0530

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import Navbar from "./components/Navbar";
 // import Portfolio from "./components/Portfolio";
 // import Contact from "./components/Contact";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 // import Skills from "./components/Skills";
 // import Experience from "./components/Experience";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const App = () => {
   return (
     <div className="bg-white h-screen ">
-      {/* <motion.div
+      <motion.div
         initial={{ opacity: 0, y: -150 }}
         animate={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         transition={{ duration: 1 }}
       >
         <Home />
-        <Skills />
+        {/* <Skills />
         <Portfolio />
         <Experience />
-        <Contact />
-      </motion.div> */}
+        <Contact /> */}
+      </motion.div>
       <Navbar />
     </div>
   );
