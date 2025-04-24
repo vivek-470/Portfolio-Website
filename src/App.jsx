ccb0c76e146c2b9992cb28de8463c4d671d829aa
vivek-470 <vk9704599@gmail.com>
2025-04-24 18:21:16 +0530

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Portfolio from "./components/Portfolio";
 // import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
-// import Experience from "./components/Experience";
+import Experience from "./components/Experience";
 import { motion } from "framer-motion";
 
 const App = () => {
@@ -19,8 +19,8 @@ const App = () => {
         <Home />
         <Skills />
         <Portfolio />
-        {/* <Experience />
-        <Contact /> */}
+        <Experience />
+        {/* <Contact /> */}
       </motion.div>
       <Navbar />
     </div>
---
+++ src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='h-[300px] flex flex-col items-center mt-18'>
+      <h1 className='font-bold text-4xl text-black'>Experience</h1>
+      <div className='h-[190px] rounded-[10px] w-[400px] md:w-[540px] border-2 border-purple-900 p-4 mt-8 bg-purple-500 text-white'>
+        <h1 className='text-2xl  font-bold'>Sqilco</h1>
+        <h1 className='text-xl py-2'>2025-present</h1>
+        <h1 className='text-xl mt-5'>I have completed  6 months of internship program  in sqilco.</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default Experience
