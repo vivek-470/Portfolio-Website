0eef1237b74ecae991d9c52be9da6784ab6c83de
vivek-470 <vk9704599@gmail.com>
2025-05-15 22:44:11 +0530

    final commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,7 +15,7 @@ const Portfolio = () => {
       </div>
       <div className=' mt-7 h-[300px] flex justify-center  flex-col gap-7 '>
        <h1 className='font-semibold text-2xl'>Project #1</h1>
-       <h1 className='font-semibold text-2xl'>Spotify clone using React</h1>
+       <h1 className='font-semibold text-2xl'>Spotify clone using React and tailwind css</h1>
        <div className='flex gap-5 '>
         <a href='https://spotify-clone-01-lyart.vercel.app/'><button className='h-[50px] w-[120px] bg-purple-500 rounded-2xl text-white font-semibold cursor-pointer hover:bg-purple-600'>View Site</button></a>
         <a href='https://github.com/vivek-470/Spotify-clone.git'><button className='h-[50px] w-[50px] bg-purple-500 rounded-2xl text-white font-semibold flex justify-center items-center cursor-pointer hover:bg-purple-600'><FaGithub className='text-2xl '/></button></a>
@@ -28,15 +28,15 @@ const Portfolio = () => {
      
       <div className=' mt-7 h-[300px] flex justify-center  flex-col gap-7 '>
        <h1 className='font-semibold text-2xl'>Project #2</h1>
-       <h1 className='font-semibold text-2xl'>Spotify clone using React</h1>
+       <h1 className='font-semibold text-2xl'>Shopping website using  React and tailwind css</h1>
        <div className='flex gap-5 '>
-        <button className='h-[50px] w-[120px] bg-purple-500 rounded-2xl text-white font-semibold cursor-pointer hover:bg-purple-600'>View Site</button>
-        <button className='h-[50px] w-[50px] bg-purple-500 rounded-2xl text-white font-semibold flex justify-center items-center cursor-pointer hover:bg-purple-600'><FaGithub className='text-2xl '/></button>
+        <a href='https://shopcart-clone-umy2.vercel.app/'><button className='h-[50px] w-[120px] bg-purple-500 rounded-2xl text-white font-semibold cursor-pointer hover:bg-purple-600'>View Site</button></a>
+        <a href="https://github.com/vivek-470/Shopcart-clone.git"><button className='h-[50px] w-[50px] bg-purple-500 rounded-2xl text-white font-semibold flex justify-center items-center cursor-pointer hover:bg-purple-600'><FaGithub className='text-2xl '/></button></a>
        </div>
 
       </div>
       <div className=' mt-[-80px] h-[450px] w-[350px] md:w-[500px] '>
-        <img className="md:h-[550px] h-[450px] md:w-[500px] w-[350px] object-contain rounded-sm " src={spotify} alt="" />
+        <img className="md:h-[550px] h-[450px] md:w-[500px] w-[350px] object-contain rounded-sm " src="https://res.cloudinary.com/dzmfopf1b/image/upload/v1747328692/shopcart_yed5c0.png" alt="" />
 
       </div>
       </div>
